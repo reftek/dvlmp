d0acca44009f3329152567357a89de86e621d0d0
Raymond Ativie <RaymondAtivie@gmail.com>
2019-01-22 09:26:42 +0100

    small cahnges

--- src/views/customer/CustomerOrders.vue
+++ src/views/customer/CustomerOrders.vue
@@ -1,3 +1,19 @@
 <template>
-    <h1>Customer Orders</h1>
+    <div>
+        <h2 class="mb-4">Order History</h2>
+
+        <div class="order-item"></div>
+    </div>
 </template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    .order-item{
+        
+    }
+</style>
