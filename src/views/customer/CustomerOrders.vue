b2069f4884412e6d31bf4bebaeb73b33455753da
Gideon Peters <gideonpeters85@gmail.com>
2019-01-22 11:05:38 +0100

    order box done

--- src/views/customer/CustomerOrders.vue
+++ src/views/customer/CustomerOrders.vue
@@ -2,7 +2,35 @@
     <div>
         <h2 class="mb-4">Order History</h2>
 
-        <div class="order-item"></div>
+        <div class="order-contain p-3 shadow-sm flex mb-2" v-for="item in 4" :key="item">
+            <div class="row">
+                <div class="col">
+                    <div class="order-item">ORD1234</div>
+                </div>
+                <div class="col date">
+                    3 days ago
+                </div>
+            </div>
+            <div class="row align-items-center mt-2">
+                <div class="col-8">
+                    <div class="row align-items-center">
+                        <div class="col-5">
+                            <div class="order-img"></div>
+                        </div>
+                        <div class="col pl-1 order-detail">
+                            <div class="row">Portal Deliveries</div>
+                            <div class="row">Item Description</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-4 text-right">
+                    <div class="delivery-status px-2">
+                        Delivered
+                    </div>
+                </div>
+            </div>
+          </div>
     </div>
 </template>
 
@@ -13,7 +41,49 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    .order-contain{
+        background-color: white;
+        border-radius: 10px;
+        // min-height: 70px;
+
+        .date{
+            text-align: right;
+            font-size: 0.7rem;
+        }
+
+        .order-img{
+            // align-self: left;
+            background-color: green;
+            height: 50px;
+            width: 85%;
+            
+        }
+
+        .order-detail{
+            font-size: 0.8rem;
+
+            div:nth-child(1) {
+                color: black;
+            }
+        }
+
+        .delivery-status{
+            // background-color: green;
+            // float: right;
+            background-color: green;
+            color: white;
+            font-size: 0.6rem;
+            display: inline-block;
+            // max-height: 5px;
+            // max-width: 10px;
+        }
+    }
+
+    h2{
+        color: black;
+    }
     .order-item{
-        
+        font-size: 0.9rem;
+        color: black; 
     }
 </style>
