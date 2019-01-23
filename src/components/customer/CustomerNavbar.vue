7e02d6080d0edafd81510a864acf4ab86eba50a2
Raymond Ativie <RaymondAtivie@gmail.com>
2019-01-23 14:50:01 +0100

    navbar edit

--- src/components/customer/CustomerNavbar.vue
+++ src/components/customer/CustomerNavbar.vue
@@ -1,14 +1,38 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-white py-4 px-4 align-center">
-        <div class="input-group w-50">
-            <i class="mdi mdi-magnify pt-2"></i>
-            <input type="text" class="form-control hide-outline hide-input" placeholder="Search Deliveries, Order Numbers">
-        </div>
+    <!-- <nav class="navbar navbar-expand-lg navbar-light bg-white py-4 px-4 align-center"> -->
         
-        <div class="navbar-nav ml-auto">
-            <a class="nav-item nav-link" active-class="active" exact :to="{name: 'main.home'}">
-                <i class="mdi mdi-bell"></i>
-            </a>
+        <div class="row justify-content-between bg-transparent px-3 py-3 align-items-center">
+            <div class="col">
+                <div class="user-image"></div>
+            </div>
+            <div class="col text-center brand-text text-primary">
+                DVMLP
+            </div>
+            <div class="col text-right">
+                <div class="btn-icon mx-2">
+                    <i class="mdi mdi-magnify"></i>
+                </div>
+                <div class="btn-icon mx-2">
+                    <i class="mdi mdi-bell"></i>
+                </div>
+            </div>
         </div>
-    </nav>
+    <!-- </nav> -->
 </template>
+
+<style lang="scss" scoped>
+.brand-text{
+    font-weight: bold;
+}
+.btn-icon{
+    display: inline-block;
+    background-color: transparent;
+}
+.user-image{
+    width: 50px;
+    height: 50px;
+    background-image: url('https://placehold.it/80');
+    background-position: center center;
+    border-radius: 50%;
+}
+</style>
