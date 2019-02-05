85fcc29a64836bc6d3df0fbea7fd18a256591ab5
Gideon Peters <gideonpeters85@gmail.com>
2019-02-05 20:25:16 +0100

    made some changes

--- src/components/customer/CustomerSidebar.vue
+++ src/components/customer/CustomerSidebar.vue
@@ -1,8 +1,23 @@
 <template>
     <nav id="sidebar" class="bg-white text-dark">
-        <router-link tag="div" class="pointer mt-5 fs-13 font-weight-bold pl-4" :to="{name: 'main.home'}">
+        <!-- <router-link tag="div" class="pointer mt-5 fs-13 font-weight-bold pl-4" :to="{name: 'main.home'}">
             DVLMP
-        </router-link>
+        </router-link> -->
+        <div class="pad-left mt-5 ">
+            <div class="row align-items-center">
+                <div class="col-4 ">
+                    <div class="profile-pic">
+
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="profile-name">
+                    Raymond Ativie 
+                    </div>
+                </div>
+            </div>
+        </div>
+        
     
         <div class="mt-5">
             <router-link class="nav-side-link py-3 my-2" active-class="active" exact :to="{name: 'customers.dashboard'}">
@@ -65,4 +80,24 @@
     text-decoration: none;
 }
 
+.pad-left{
+    padding-left: 40px;
+}
+
+.profile-pic{
+    background-image: url('./../../assets/images/iStock-1019214052.png');
+    background-position: center, center;
+    background-repeat: no-repeat;
+    min-height: 60px;
+    min-width: 60px;
+    border-radius: 50%;
+    position: relative;
+    top: 0.3rem;
+}
+
+.profile-name{
+    position: relative;
+    right: 1rem;
+    font-weight: bold;
+}
 </style>
